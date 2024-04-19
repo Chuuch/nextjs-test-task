d51246dfd0ddbdb461c67337a342c6ffc76bed72
Chuuch <daniel.chuchulev96@gmail.com>
2024-04-19 15:48:59 +0300

    refactored home app component and added Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+/* eslint-disable @next/next/no-page-custom-font */
+import Hero from "../sections/Hero/Hero";
 
+interface HeroProps {
+  image: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+  title: string;
+  description: string;
+  ctaText: string;
+}
+
+const heroProps: HeroProps = {
+  image: {
+    src: "/img/heroimg.png",
+    alt: "hero",
+    width: 1000,
+    height: 1680,
+  },
+  title: "Agency procurement, outsourced.",
+  description: "Start the process here",
+  ctaText: "Start",
+};
 
 export default function Home() {
   return (
-   <>
-
-   </>
+    <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <div>
+        <Hero {...heroProps} />
+      </div>
+    </>
   );
 }
